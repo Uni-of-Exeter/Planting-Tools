3b44648bba9cabf09b61d42d5c40b41206f34d84
Timothee Bacri <t.r.f.bacri@exeter.ac.uk>
2024-11-22 12:54:48 +0000

    new messages when creating FullTable

--- ShinyForestry/app.R
+++ ShinyForestry/app.R
@@ -393,6 +393,8 @@ if (!file.exists(normalizePath(file.path(ElicitorAppFolder, "FullTableMerged.geo
   }
   
   # Replace Biodiversity columns with correct ones
+  msg <- "Converting biodiversity from square grid cells to our shapefile polygons ..."
+  notif(msg, log_level = "info")
   FullTable <- convert_bio_to_polygons_from_elicitor_and_merge_into_FullTable(Elicitor_table = FullTable,
                                                                               speciesprob_list = speciesprob_list,
                                                                               seer2km = seer2km,
@@ -400,6 +402,8 @@ if (!file.exists(normalizePath(file.path(ElicitorAppFolder, "FullTableMerged.geo
                                                                               climatecells = climatecells,
                                                                               MAXYEAR = MAXYEAR,
                                                                               global_log_level = LOG_LEVEL)
+  msg <- paste(msg, "done")
+  notif(msg, log_level = "info")
   # Free a lot of RAM
   rm(speciesprob_list)
   
