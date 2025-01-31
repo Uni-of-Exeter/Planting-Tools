dd1c2c947b74677796c3623301fe176434cdea92
Timothee Bacri <t.r.f.bacri@exeter.ac.uk>
2025-01-31 09:38:51 +0000

    fix paths

--- ShinyForestry/AllResults/ScriptToRun.R
+++ ShinyForestry/AllResults/ScriptToRun.R
@@ -1,22 +1,26 @@
 require(adaptMCMC)
 
 TOTAL_USER_NB<-9
-FolderSource<-paste0(getwd(),"\\ShinyForestry")
+FolderSource<-normalizePath(file.path(getwd(),"ShinyForestry"))
 #FolderSource<-"C:/Users/bn267/OneDrive - University of Exeter/Documents/GitHub/Planting-Tools/ShinyForestry"
 
-source(paste0(FolderSource, "//preferTrees.R"), local = FALSE)
-BasePrefObj<-readRDS(paste0(FolderSource,"\\AllResults\\BasePrefObj.RDS"))
+source(normalizePath(file.path(FolderSource, "preferTrees.R")), local = FALSE)
+BasePrefObj<-readRDS(normalizePath(file.path(FolderSource,"AllResults", "BasePrefObj.RDS")))
 
-L1<-readRDS(paste0(FolderSource,"\\AllResults\\pref_LIST1.RData"))
+L1<-readRDS(normalizePath(file.path(FolderSource,"AllResults", "pref_LIST1.RData")))
 DATT<-L1$data
 PREFF<-L1$prefs
 
 for(ii in 2:TOTAL_USER_NB){
-Lnext<-readRDS(paste0(FolderSource,"\\AllResults\\pref_LIST",ii,".RData"))
+# Lnext<-readRDS(paste0(FolderSource,"\\AllResults\\pref_LIST",ii,".RData"))
+file <- normalizePath(file.path(FolderSource,
+                                "AllResults",
+                                paste0("pref_LIST", ii, ".RData")))
+Lnext<-readRDS(file)
 CurrentRows<-dim(DATT)[1]
 DATT<-rbind(DATT,Lnext$data)
 PREFF<-rbind(PREFF,Lnext$prefs+CurrentRows)
 }
 
 NewObj<-prefObject(priors = BasePrefObj$priors,data=DATT,prefs=PREFF)
-saveRDS(NewObj,paste0(FolderSource,"\\FixedStrats\\pref_reactive.RDS"))
+saveRDS(NewObj,normalizePath(file.path(FolderSource,"FixedStrats", "pref_reactive.RDS")))
