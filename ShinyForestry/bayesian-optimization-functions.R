7f06f204e6f4179801cc7aab78e83f285d2306ba
Timothee Bacri <t.r.f.bacri@exeter.ac.uk>
2025-01-31 09:16:14 +0000

    add time to notification messages

--- ShinyForestry/bayesian-optimization-functions.R
+++ ShinyForestry/bayesian-optimization-functions.R
@@ -1854,7 +1854,7 @@ notif <- function(msg,
   if (log_level > limit_log_level) return()
   
   if (isFALSE(rbind)) {
-    msg <- paste0(Sys.time(), " [", log_level_msg, "] ", msg)
+    msg <- paste0("[", log_level_msg, "] ", Sys.time(), " ", msg)
   }
   
   pad_notif_message <- function(msg, pad_character = "_") {
