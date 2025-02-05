282df793fe0e59a498007c5e50fbb8bc6dbfb23b
Timothee Bacri <t.r.f.bacri@exeter.ac.uk>
2025-02-05 11:59:29 +0000

    More robust file listing in ScriptToRun.R

--- ShinyForestry/AllResults/ScriptToRun.R
+++ ShinyForestry/AllResults/ScriptToRun.R
@@ -7,20 +7,31 @@ FolderSource<-normalizePath(file.path(getwd(),"ShinyForestry"))
 source(normalizePath(file.path(FolderSource, "preferTrees.R")), local = FALSE)
 BasePrefObj<-readRDS(normalizePath(file.path(FolderSource,"AllResults", "BasePrefObj.RDS")))
 
-L1<-readRDS(normalizePath(file.path(FolderSource,"AllResults", "pref_LIST1.RData")))
-DATT<-L1$data
-PREFF<-L1$prefs
+files <- list.files(normalizePath(file.path(FolderSource,"AllResults")), "pref_LIST.*[.]RData")
 
-for(ii in 2:TOTAL_USER_NB){
-# Lnext<-readRDS(paste0(FolderSource,"\\AllResults\\pref_LIST",ii,".RData"))
-file <- normalizePath(file.path(FolderSource,
-                                "AllResults",
-                                paste0("pref_LIST", ii, ".RData")))
-Lnext<-readRDS(file)
-CurrentRows<-dim(DATT)[1]
-DATT<-rbind(DATT,Lnext$data)
-PREFF<-rbind(PREFF,Lnext$prefs+CurrentRows)
+DATT <- data.frame()
+PREFF <- data.frame()
+for (file in files) {
+  pref <- readRDS(file)
+  CurrentRows<-dim(DATT)[1]
+  DATT<-rbind(DATT,pref$data)
+  PREFF<-rbind(PREFF,pref$prefs+CurrentRows)
 }
+
+# L1<-readRDS(normalizePath(file.path(FolderSource,"AllResults", "pref_LIST1.RData")))
+# DATT<-L1$data
+# PREFF<-L1$prefs
+# 
+# for(ii in 2:TOTAL_USER_NB){
+# # Lnext<-readRDS(paste0(FolderSource,"\\AllResults\\pref_LIST",ii,".RData"))
+# file <- normalizePath(file.path(FolderSource,
+#                                 "AllResults",
+#                                 paste0("pref_LIST", ii, ".RData")))
+# Lnext<-readRDS(file)
+# CurrentRows<-dim(DATT)[1]
+# DATT<-rbind(DATT,Lnext$data)
+# PREFF<-rbind(PREFF,Lnext$prefs+CurrentRows)
+# }
 
 NewObj<-prefObject(priors = BasePrefObj$priors,data=DATT,prefs=PREFF)
 saveRDS(NewObj,normalizePath(file.path(FolderSource,"FixedStrats", "pref_reactive.RDS")))
