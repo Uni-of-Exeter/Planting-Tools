4bb2eff6c785e27c62a25857f435ba9eebd0c91e
Timothée Bacri <t.r.f.bacri@exeter.ac.uk>
2025-02-11 14:33:40 +0000

    remove commented code

--- ShinyForestry/app.R
+++ ShinyForestry/app.R
@@ -2577,40 +2577,6 @@ server <- function(input, output, session,
       
       
     }
-    
-    # TODO: Fix this
-    #     if (input$tabs == "Alternative Approaches") {
-    #       runjs('
-    #    var widget = HTMLWidgets.find("#map2");
-    #       widget.resize($("#map2")[0], $("#map2").width(), $("#map2").height());
-    # ')
-    #       
-    #       runjs('
-    #    var widget = HTMLWidgets.find("#map3");
-    #       widget.resize($("#map3")[0], $("#map3").width(), $("#map3").height());
-    # ')
-    #       runjs('
-    #    var widget = HTMLWidgets.find("#map4");
-    #       widget.resize($("#map4")[0], $("#map4").width(), $("#map4").height());
-    # ')
-    #     }
-    
-    
-    # TODO: Fix this
-    #     if (input$tabs == "Exploration") {
-    #       runjs('
-    #    var widget = HTMLWidgets.find("#map6");
-    #       widget.resize($("#map6")[0], $("#map6").width(), $("#map6").height());
-    #        $("#map6").data("leaflet").invalidateSize();
-    # ')
-    #       runjs('
-    #    var widget = HTMLWidgets.find("#Full-elements-container);
-    #       widget.resize($("#Full-elements-container")[0], $("#Full-elements-container").width(), $("#Full-elements-container").height());
-    #         $("#Full-elements-container").data("leaflet").invalidateSize();
-    # ')
-    #     }
-    
-    
   })
   
   # On session close, delete temporary files
