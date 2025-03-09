ae3d925e7fcbd8ca79e153fe3ba38f71a5851cb7
Paul Wright <paul@pauljwright.co.uk>
2025-03-09 14:46:28 +0000

    reduce scope to first two tabs

--- ShinyForestry/multi_tab_app.R
+++ ShinyForestry/multi_tab_app.R
@@ -60,9 +60,9 @@ ui <- page_navbar(
   
   tabPanel(title = "Map", map_page_ui("map")),
   tabPanel(title = "Preferences", preferences_page_ui("prefs")),
-  tabPanel(title = "Alternative Approaches", alternative_approaches_page_ui("alt") ),
-  tabPanel(title = "Exploration", exploration_page_ui("explore")),
-  tabPanel(title = "Downscaling", downscaling_page_ui("downscale")),
+  # tabPanel(title = "Alternative Approaches", alternative_approaches_page_ui("alt") ),
+  # tabPanel(title = "Exploration", exploration_page_ui("explore")),
+  # tabPanel(title = "Downscaling", downscaling_page_ui("downscale")),
   
   nav_spacer(),
   nav_item(tags$a("AI for Net Zero",
