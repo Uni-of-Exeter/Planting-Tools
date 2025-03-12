65c76578ab46d039f38c73fcc6c3caeb5badb227
Timothee Bacri <t.r.f.bacri@exeter.ac.uk>
2025-03-12 12:45:52 +0000

    call the proper Plumber backend api file

--- ShinyForestry/backend/trigger_plumber_for_dev.R
+++ ShinyForestry/backend/trigger_plumber_for_dev.R
@@ -3,10 +3,10 @@ port <- Sys.getenv("API_PORT")
 if (port == "") {
   port <- 40000
 }
-if (file.exists(file.path(getwd(), "ShinyForestry", "backend", "mock_strategy.R"))) {
-  path <- normalizePath(file.path(getwd(), "ShinyForestry", "backend", "mock_strategy.R"))
+if (file.exists(file.path(getwd(), "ShinyForestry", "backend", "plumber_apis.R"))) {
+  path <- normalizePath(file.path(getwd(), "ShinyForestry", "backend", "plumber_apis.R"))
 } else {
-  path <- normalizePath(file.path(getwd(), "mock_strategy.R"))
+  path <- normalizePath(file.path(getwd(), "plumber_apis.R"))
 }
 # options(plumber.apiHost = "0.0.0.0")
 plumb(path) |>
