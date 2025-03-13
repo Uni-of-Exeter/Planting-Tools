7abb90251af63f1d7dbcd2d139e807ac76c56a42
Timothee Bacri <t.r.f.bacri@exeter.ac.uk>
2025-03-13 14:31:35 +0000

    target_compatible_strategies is handled outside the function, returned then merged to .GlobalEnv

--- backend/DannySamples.R
+++ backend/DannySamples.R
@@ -476,12 +476,12 @@ cluster_samples <- function(){
   #CANT CLUSTER IF NOT ENOUGH SAMPLES. 
   if(nrow(target_compatible_strategies)<6){
     notif("Too few target compatible samples to cluster")
-    target_compatible_strategies <<- target_compatible_strategies[,cluster := 1]
+    target_compatible_strategies <- target_compatible_strategies[,cluster := 1]
     return(invisible())
   }
     tsne_projected_target_compatible_data <- Rtsne::Rtsne(scale(target_compatible_strategies[,..TARGETS], center=FALSE, scale = sqrt(abs(preference_weights))), perplexity = min(30, (nrow(target_compatible_strategies)-1.01)/3))$Y
     clustered_samples <- mclust::Mclust(tsne_projected_target_compatible_data,G=4)
-    target_compatible_strategies <<- target_compatible_strategies[,cluster := clustered_samples$classification]
+    target_compatible_strategies <- target_compatible_strategies[,cluster := clustered_samples$classification]
     unique_clusters <- 1:4
     cluster_projections <- lapply(unique_clusters, function(ZZ) {
       projected <- tsne_projected_target_compatible_data %*% eigen(clustered_samples$parameters$variance$sigma[,,ZZ])$vectors
@@ -493,6 +493,8 @@ cluster_samples <- function(){
       row_index <- .I  # Row index for correct alignment
       list(proj[row_index, 1], proj[row_index, 2])  # Assign correct rows
     }, by=cluster]
+    
+    return(target_compatible_strategies)
 }
 cluster_samples()
 
