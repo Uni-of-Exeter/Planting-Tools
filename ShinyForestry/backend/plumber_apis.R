afb58c1ffab5a94ce4da6059a1c7636ddef2ddf3
BayesExeter <d.williamson@exeter.ac.uk>
2025-03-14 20:32:26 +0000

    Pass back right labels to Front end: biodiversity --> Biodiversity carbon --> Carbon area --> Area visits --> Food_Produced

--- ShinyForestry/backend/plumber_apis.R
+++ ShinyForestry/backend/plumber_apis.R
@@ -402,9 +402,11 @@ function(req, res, from_submit_button) {
   geojson <- geojsonsf::sf_geojson(for_frontend)
   
   payload <- optimal_strategy_forfrontend[, ..TARGETS]
-  names(payload)[which(names(payload)=="All")] <- "biodiversity"
-  names(payload)[which(names(payload)=="visits")] <- "recreation"
-  bio_names_latin <- names(payload)[ ! names(payload)%in% c("carbon", "area", "recreation", "biodiversity")]
+  names(payload)[which(names(payload)=="All")] <- "Biodiversity"
+  names(payload)[which(names(payload)=="visits")] <- "Food_Produced"
+  names(payload)[which(names(payload)=="area")] <- "Area"
+  names(payload)[which(names(payload)=="carbon")] <- "Carbon"
+  bio_names_latin <- names(payload)[ ! names(payload)%in% c("Carbon", "Area", "Food_Produced", "Biodiversity")]
   bio_names_latin
   for(species in bio_names_latin){
     specie_num <- which(NAME_CONVERSION$Specie == species)
